513175b2783c3fd376dcbe700a26ed35f0c337cf
Koch13o1 <maitreyakoch123@gmail.com>
2025-02-28 14:42:44 -0500

    Hyperparameter tuning done

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -20,16 +21,26 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for model_name, model in models.items():
-            if 'Cat' in model_name:
-                model.fit(X_train, y_train, verbose=0)  # Train Model
-            else:
-                model.fit(X_train, y_train)  # Train Model
+            param = params[model_name]
             
+            if 'Cat' in model_name:
+                gs = GridSearchCV(model, param, cv=3, verbose=0)
+                gs.fit(X_train, y_train, verbose=0)
+                # model.fit(X_train, y_train, verbose=0)  # Train Model
+            else:
+                gs = GridSearchCV(model, param, cv=3)
+                gs.fit(X_train, y_train)
+                # model.fit(X_train, y_train)  # Train Model
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
