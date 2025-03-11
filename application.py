2efe303435eec933fcb803904d225a0391c6f5fb
Koch13o1 <maitreyakoch123@gmail.com>
2025-03-11 14:50:38 -0400

    added logging to application.py

--- application.py
+++ application.py
@@ -15,25 +15,29 @@ def index():
 
 @application.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        data=CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('race_ethnicity'),
-            parental_level_of_education = request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = float(request.form.get('reading_score')),
-            writing_score = float(request.form.get('writing_score'))
-        )
+    try:
+        if request.method == 'GET':
+            return render_template('home.html')
+        else:
+            data=CustomData(
+                gender = request.form.get('gender'),
+                race_ethnicity = request.form.get('race_ethnicity'),
+                parental_level_of_education = request.form.get('parental_level_of_education'),
+                lunch = request.form.get('lunch'),
+                test_preparation_course = request.form.get('test_preparation_course'),
+                reading_score = float(request.form.get('reading_score')),
+                writing_score = float(request.form.get('writing_score'))
+            )
 
-        pred_df = data.get_data_as_data_frame()
-        # print(pred_df)
+            pred_df = data.get_data_as_data_frame()
+            # print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=min(100, results[0]))
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html', results=min(100, results[0]))
+    except Exception as e:
+        application.logger.error(f"Error in predict_datapoint: {e}", exc_info=True)
+        return "Internal Server Error", 500
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0")
